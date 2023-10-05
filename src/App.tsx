469fe18b412c364ba89e0709ca648127913a91b1
Ling <lingxin_chen@sil.org>
2023-10-05 14:49:31 -0500

    Redesign the frontpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+//import '../public/banner.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>platform.Bible</p>
-        <p>coming soon</p>
+        <p>Platform.Bible</p>
       </header>
+      <main className="main-content">
+        <h2>Main Content</h2>
+        <p>This is the main content section below the header.</p>
+        <p>You can add more content here.</p>
+      </main>
     </div>
   );
 }
