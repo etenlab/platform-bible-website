23cfd645553c15f015e7257ce11f72399fe2d45e
Ling <lingxin_chen@sil.org>
2023-10-06 10:06:34 -0500

    Add logo and remove main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-//import '../public/banner.jpg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src="/platformCube.png" alt="Logo" className="logo" />
         <p>Platform.Bible</p>
       </header>
-      <main className="main-content">
-        <h2>Main Content</h2>
-        <p>This is the main content section below the header.</p>
-        <p>You can add more content here.</p>
-      </main>
     </div>
   );
 }
